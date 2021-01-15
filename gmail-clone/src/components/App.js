4ff7d816f7e267ba66a8723c7ad4aa383290075e
TamTran72111 <tam.tran72111@gmail.com>
2021-01-15 09:53:25 -0500

    Add Google Authentication

--- gmail-clone/src/components/App.js
+++ gmail-clone/src/components/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 import Header from './Header';
@@ -7,31 +7,57 @@ import Sidebar from './Sidebar';
 import Mail from './Mail';
 import EmailList from './EmailList';
 import SendMail from './SendMail';
+import Login from './Login';
 import { selectSendMessageIsOpen } from '../features/mailSlice';
+import { login, logout, selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
 import './App.css';
 
 function App() {
   const sendMessageIsOpen = useSelector(selectSendMessageIsOpen);
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <div className="app__body">
-          <Sidebar />
-          <Switch>
-            <Route path="/mail">
-              <Mail />
-            </Route>
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
-            <Route path="/">
-              <EmailList />
-            </Route>
-          </Switch>
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+
+  if (user) {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Switch>
+              <Route path="/mail">
+                <Mail />
+              </Route>
+
+              <Route path="/">
+                <EmailList />
+              </Route>
+            </Switch>
+          </div>
+          {sendMessageIsOpen && <SendMail />}
         </div>
-        {sendMessageIsOpen && <SendMail />}
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  } else {
+    return <Login />;
+  }
 }
 
 export default App;
--- gmail-clone/src/components/Header.js
+++ gmail-clone/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import AppsIcon from '@material-ui/icons/Apps';
 import MenuIcon from '@material-ui/icons/Menu';
 import NotificationsIcon from '@material-ui/icons/Notifications';
@@ -6,9 +7,20 @@ import SearchIcon from '@material-ui/icons/Search';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import { Avatar, IconButton } from '@material-ui/core';
 
+import { logout, selectUser } from '../features/userSlice';
+import { auth } from '../firebase';
 import './Header.css';
 
 const Header = () => {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  const signOut = () => {
+    auth.signOut().then(() => {
+      dispatch(logout());
+    });
+  };
+
   return (
     <div className="header">
       <div className="header__left">
@@ -34,7 +46,7 @@ const Header = () => {
         <IconButton>
           <NotificationsIcon />
         </IconButton>
-        <Avatar />
+        <Avatar onClick={signOut} src={user?.photoUrl} />
       </div>
     </div>
   );
